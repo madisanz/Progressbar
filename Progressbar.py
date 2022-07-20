85e49f9eb17aa5f5294a3a19c80726cde9dc84a8
Margarita Sánchez <57296098+madisanz@users.noreply.github.com>
2022-07-20 11:52:34 -0300

    Update Progressbar.py

--- Progressbar.py
+++ Progressbar.py
@@ -1,5 +1,16 @@
+
 # Progress Bar
+
 from tqdm.auto import tqdm 
 
 for i in tqdm(range(100001)):
     print(" ", end='\r')
+  
+
+# With tqdm (conda install tqdm or pip install tqdm) 
+# I can add a progress meter to my loops in a second:
+
+from time import sleep
+from tqdm import tqdm
+for i in tqdm(range(10)):
+    sleep(3)
